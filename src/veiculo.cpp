c5b9e6f334eb0ff6b87e4e749c2c7f5d554d7079
Jady Lima <jadylsilva@gmail.com>
2022-11-25 11:18:24 -0300

    Create veiculo.cpp

---
+++ src/veiculo.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "veiculo.h"
+
+Veiculo::Veiculo(string ch, string m, double p, int a) : Concessionaria(estoque, nome, CNPJ)
+{
+    chassi = ch;
+    marca = m;
+    preco = p;
+    ano_fabricacao = a;
+}
+
+Veiculo::~Veiculo()
+{
+}
