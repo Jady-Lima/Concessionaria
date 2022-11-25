25d5cc380a0a879c9a440ca867fbc18d082cce7f
Jady Lima <jadylsilva@gmail.com>
2022-11-25 10:44:30 -0300

    Create sistema.cpp

---
+++ src/sistema.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <sstream>
+#include <algorithm>
+
+#include "sistema.h"
+
+//CONSTRUTOR DA CLASSE
+Sistema::Sistema(string arg)
+{
+    //quebra o argumento recebido e descobre qual função chamar(criar, remover, add, buscar)
+}
+
+string Sistema::create_concessionaria (const string nome, string CNPJ, int estoque)
+{
+    //cria a concessionaria
+}
+
+//ENCERRA O PROGRAMA
+string quit()
+{
+    return "Saindo...";
+}
+
+//DESTRUTOR DA CLASSE SISTEMA
+Sistema::~Sistema()
+{
+    
+}
