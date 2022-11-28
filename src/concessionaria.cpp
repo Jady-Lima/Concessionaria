e5d8ee30d2a3aaa5a5a8e40fc6acc37cc39bc5c2
Jady Lima <jadylsilva@gmail.com>
2022-11-28 10:35:01 -0300

    Update concessionaria.cpp

--- src/concessionaria.cpp
+++ src/concessionaria.cpp
@@ -1,11 +1,17 @@
 #include "../include/concessionaria.h"
+#include "../include/veiculo.h"
 
 Concessionaria::Concessionaria(string n, string c, string e)
 {
     estoque = e;
     nome = n;
     CNPJ = c;
-    cout << "Consecionaria ok " << endl;
+
+    vector<Veiculo> v_car;
+    vector<Veiculo> v_truck;
+    vector<Veiculo> v_moto;
+
+    cout << "Concessionaria ok " << endl;
 }
 
 Concessionaria::~Concessionaria()
